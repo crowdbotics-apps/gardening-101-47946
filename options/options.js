56f8a13246022000e70e3b4048896d35c65035e1
Crowdbotics <team@crowdbotics.com>
2024-05-15 04:03:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gardening-101-47946",
+  url: "https://gardening-101-47946.botics.co",
+  api: "https://gardening-101-47946.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
